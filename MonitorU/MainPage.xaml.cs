7faf9f4fffd4e5d0b6ee5e44a35218ac43fd431a
Marc Karassev <vasskera@hotmail.fr>
2015-11-14 13:53:46 +0100

    1st commit

---
+++ MonitorU/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using MonitorU.Resources;
+
+namespace MonitorU
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructeur
+        public MainPage()
+        {
+            System.Diagnostics.Debug.WriteLine("Hey, I just met you!");
+            InitializeComponent();
+
+            // Exemple de code pour la localisation d'ApplicationBar
+            //BuildLocalizedApplicationBar();
+            
+            PhoneApplicationService.Current.ApplicationIdleDetectionMode = IdleDetectionMode.Disabled;
+            PhoneApplicationFrame rootFrame = App.Current.RootVisual as PhoneApplicationFrame;
+            if (rootFrame != null)
+            {
+                rootFrame.Obscured += OnObscured;
+                rootFrame.Unobscured += Unobscured;
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            PhoneApplicationFrame rootFrame = App.Current.RootVisual as PhoneApplicationFrame;
+            if (rootFrame != null)
+            {
+                System.Diagnostics.Debug.WriteLine("And this is craaaaazy!");
+                rootFrame.Obscured += OnObscured;
+                rootFrame.Unobscured += Unobscured;
+            }
+        }
+
+        void OnObscured(Object sender, ObscuredEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Here's my number.");
+            txtObs.Text = "Obscured at " + DateTime.Now.ToString();
+        }
+
+        void Unobscured(Object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("So call me maybe!");
+            txtUnobs.Text = "Unobscured at " + DateTime.Now.ToString();
+        }
+        // Exemple de code pour la conception d'une ApplicationBar localisée
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Définit l'ApplicationBar de la page sur une nouvelle instance d'ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Crée un bouton et définit la valeur du texte sur la chaîne localisée issue d'AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Crée un nouvel élément de menu avec la chaîne localisée d'AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
